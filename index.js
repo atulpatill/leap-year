7e9826d02f2ada93eaa88713e79bec4fd60fc1b2
Atul Patil <atulpatil539@gmail.com>
2021-09-02 15:27:38 +0530

    feat:added

---
+++ index.js
@@ -0,0 +1,10 @@
+const clickBtn = document.querySelector("#btn")
+
+function eventHandler(){
+    console.log("date is not in leap year")
+}
+
+
+
+clickBtn.addEventListener("click", eventHandler)
+
