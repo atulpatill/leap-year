42c484adb240dfe425b56754c40d18ba7f0a2106
Atul Patil <atulpatil539@gmail.com>
2021-09-03 18:50:25 +0530

    feat:error Handling

--- index.js
+++ index.js
@@ -1,21 +1,26 @@
-const inputDate = document.querySelector("#input1");
+const textInput = document.querySelector("#text-input")
 const clickBtn = document.querySelector("#btn");
+const outputDiv = document.querySelector("#output")
 
-const date = inputDate.value
+
+
+
+// var userName = prompt("Give me your user Name")
+// alert( "This script works " + userName)
+
+function errorHandler(error){
+    console.log("error coccured", error)
+    alert("Try after some time")
+}
 
 function eventHandler(){
-    const date = inputDate.value
-
-    console.log(date)
-
-    console.log("checking the date you entered")
-
-
-    console.log("date is not in leap year")
-
-
+    
+    outputDiv.innerText = "afdgyurjhbrf " + textInput.value 
 
 }
+
+
+.catch(errorHandler)
 
 
 
